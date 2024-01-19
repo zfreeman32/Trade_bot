699c3822b2263e9857c7de246141f61bf0380cc4
zfreeman32 <zeb.freeman2000@gmail.com>
2024-01-19 00:12:18 -0600

    lotto changes

--- model_training/model_build.py
+++ model_training/model_build.py
@@ -3,132 +3,117 @@ import sys
 sys.path.append(r'C:\Users\zeb.freeman\Documents\Trade_bot')
 from sklearn.metrics import mean_squared_error
 from keras.models import Sequential, Dense
-from keras.layers import Flatten, MaxPooling1D
+from keras.layers import Flatten, MaxPooling1D, Input
 from keras.optimizers import Adam
 from layer_build import build_Dense_layer, build_LSTM_layer, build_GRU_layer, build_SimpleRNN_layer, build_Conv1D_layer, build_Dropout_layer
 
 seed = 42
 
-#%%
 # Build the LSTM model
-def build_LSTM_model(hp):
+def build_LSTM_model(hp, input_shape):
 	model = Sequential()
-
+	# Add Input layer
+	model.add(Input(shape=input_shape))
 	# Add LSTM layers based on the hyperparameters
 	for i in range(hp.Int("num_lstm_layers", min_value=1, max_value=3, step=1)):
-		model.add(build_LSTM_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2]), return_sequences=True))
-
+		model.add(build_LSTM_layer(hp, return_sequences=True))
 	# Add last LSTM
-	model.add(build_LSTM_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-		
+	model.add(build_LSTM_layer(hp))
 	# Add Dense layers based on the hyperparameters
 	for i in range(hp.Int("num_dense_layers", min_value=1, max_value=2, step=1)):
-		model.add(build_Dense_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-
-	model.add(Dense(units = 1, input_shape=(train_X.shape[1], train_X.shape[2])))
-
+		model.add(build_Dense_layer(hp))
+	model.add(Dense(units=1))
 	model.compile(optimizer=Adam(learning_rate=hp.Float("learning_rate", min_value=1e-4, max_value=1e-2, sampling="LOG")),
 					loss=mean_squared_error)
 	return model
 
-#%%
 # Build the GRU model
-def build_GRU_model(hp):
+def build_GRU_model(hp, input_shape):
 	model = Sequential()
+	# Add Input layer
+	model.add(Input(shape=input_shape))  
 	# Add LSTM layers based on the hyperparameters
 	for i in range(hp.Int("num_lstm_layers", min_value=1, max_value=3, step=1)):
-		model.add(build_GRU_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2]), return_sequences=True))
+		model.add(build_GRU_layer(hp, return_sequences=True))
 	# Add last LSTM
-	model.add(build_GRU_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
+	model.add(build_GRU_layer(hp))
 	# Add Dense layers based on the hyperparameters
 	for i in range(hp.Int("num_dense_layers", min_value=1, max_value=2, step=1)):
-		model.add(build_Dense_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-	model.add(Dense(units = 1, input_shape=(train_X.shape[1], train_X.shape[2])))
-
+		model.add(build_Dense_layer(hp))
+	model.add(Dense(units = 1))
 	model.compile(optimizer=Adam(learning_rate=hp.Float("learning_rate", min_value=1e-4, max_value=1e-2, sampling="LOG")),
 					loss=mean_squared_error)
 	return model
 
-#%%
 # Build the SimpleRNN model
-def build_SimpleRNN_model(hp):
+def build_SimpleRNN_model(hp, input_shape):
 	model = Sequential()
-
+	# Add Input layer
+	model.add(Input(shape=input_shape))  
 	# Add LSTM layers based on the hyperparameters
 	for i in range(hp.Int("num_lstm_layers", min_value=1, max_value=3, step=1)):
-		model.add(build_SimpleRNN_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2]), return_sequences=True))
-
+		model.add(build_SimpleRNN_layer(hpreturn_sequences=True))
 	# Add last LSTM
-	model.add(build_SimpleRNN_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-		
+	model.add(build_SimpleRNN_layer(hp))	
 	# Add Dense layers based on the hyperparameters
 	for i in range(hp.Int("num_dense_layers", min_value=1, max_value=2, step=1)):
-		model.add(build_Dense_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-
-	model.add(Dense(units = 1, input_shape=(train_X.shape[1], train_X.shape[2])))
-
+		model.add(build_Dense_layer(hp))
+	model.add(Dense(units = 1))
 	model.compile(optimizer=Adam(learning_rate=hp.Float("learning_rate", min_value=1e-4, max_value=1e-2, sampling="LOG")),
 					loss=mean_squared_error)
-	
 	return model
 
-#%%
 # Build Conv1D model
-def build_Conv1D_model(hp):
+def build_Conv1D_model(hp, input_shape):
     model = Sequential()
-
+    # Add Input layer
+    model.add(Input(shape=input_shape))
     # Add Conv1D layers based on the hyperparameters
     for i in range(hp.Int("num_conv1d_layers", min_value=1, max_value=3, step=1)):
-        model.add(build_Conv1D_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-		
+        model.add(build_Conv1D_layer(hp))      
     # Flatten layer before Dense layers
     model.add(Flatten())
-
     # Add Dense layers based on the hyperparameters
     for i in range(hp.Int("num_dense_layers", min_value=1, max_value=2, step=1)):
-        model.add(build_Dense_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-
-    model.add(Dense(units = 1, input_shape=(train_X.shape[1], train_X.shape[2])))
-
+        model.add(build_Dense_layer(hp))
+    model.add(Dense(units = 1))
     model.compile(optimizer=Adam(learning_rate=hp.Float("learning_rate", min_value=1e-4, max_value=1e-2, sampling="LOG")),
-                  loss=mean_squared_error)
-
+                    loss=mean_squared_error)
     return model
 
 # Conv1D model
 def build_Conv1DPooling_model(hp, input_shape):
     model = Sequential()
+    # Add Input layer
+    model.add(Input(shape=input_shape))
     # Add Conv1D layers based on the hyperparameters
     for i in range(hp.Int("num_conv1d_layers", min_value=1, max_value=3, step=1)):
         for i in range(hp.Int("num_lstm_layers", min_value=1, max_value=3, step=1)):
-            model.add(build_Conv1D_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2]), return_sequences=True))
+            model.add(build_Conv1D_layer(hp, return_sequences=True))
         model.add(MaxPooling1D(pool_size=2, strides=None, padding="valid", data_format=None))
     model.add(Flatten()),
     model.add(build_Dropout_layer(hp)),
-
     for i in range(hp.Int("num_dense_layers", min_value=1, max_value=2, step=1)):
-        model.add(build_Dense_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-    
-    model.add(Dense(units = 1, input_shape=(train_X.shape[1], train_X.shape[2])))
+        model.add(build_Dense_layer(hp))
+    model.add(Dense(units = 1))
     return model
 
 # Conv1D + LSTM model
-def build_Conv1D_LSTM_model(hp):
+def build_Conv1D_LSTM_model(hp, input_shape):
     model = Sequential()
+    # Add Input layer
+    model.add(Input(shape=input_shape))
     # Add Conv1D layers based on the hyperparameters
     for i in range(hp.Int("num_conv1d_layers", min_value=1, max_value=3, step=1)):
         for i in range(hp.Int("num_lstm_layers", min_value=1, max_value=3, step=1)):
-            model.add(build_Conv1D_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2]), return_sequences=True))
+            model.add(build_Conv1D_layer(hp, return_sequences=True))
         model.add(MaxPooling1D(pool_size=2, strides=None, padding="valid", data_format=None))
-
     if hp.Boolean("Flatten"):
         model.add(Flatten()),
     # Add LSTM layers based on the hyperparameters
     for i in range(hp.Int("num_lstm_layers", min_value=1, max_value=3, step=1)):
-        model.add(build_LSTM_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2]), return_sequences=True))
-
+        model.add(build_LSTM_layer(hp, return_sequences=True))
     for i in range(hp.Int("num_dense_layers", min_value=1, max_value=2, step=1)):
-        model.add(build_Dense_layer(hp, input_shape=(train_X.shape[1], train_X.shape[2])))
-
-    model.add(Dense(units = 1, input_shape=(train_X.shape[1], train_X.shape[2])))
+        model.add(build_Dense_layer(hp))
+    model.add(Dense(units = 1))
     return model
