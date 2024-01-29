dc8e06938b50e5f20e57d8c9298537e163de2750
zfreeman32 <zeb.freeman2000@gmail.com>
2024-01-28 19:27:42 -0600

    data split

--- model_training/model_train.py
+++ model_training/model_train.py
@@ -11,9 +11,9 @@ import ta
 from matplotlib import pyplot
 from Strategies import call_Strategies
 from model_training import preprocess_data
-import keras_tuner as kt
-from keras_tuner import HyperParameters
-from model_training import model_build
+# import keras_tuner as kt
+# from keras_tuner import HyperParameters
+# from model_training import model_build
 seed = 42
 
 #%%
@@ -49,7 +49,7 @@ reframed_data
 #%%
 # split into train and test sets
 values = reframed_data.values
-n_total_days = 1000
+n_total_days = 6000
 n_train_days = int(n_total_days * 0.8)
 # Use the last 1000 days
 data_subset = values[-n_total_days:, :]
@@ -64,6 +64,24 @@ train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
+#%%
+from sklearn.model_selection import train_test_split
+
+# Assuming values is your dataset
+# split into train and test sets
+values = reframed_data.values
+# split into train and test sets without shuffling
+train, test = train_test_split(values, test_size=0.2, shuffle=False)
+
+# split into input and outputs
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+
+# reshape input to be 3D [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 #%%
 # Hyperparameter tuning using RandomSearch from Kerastuner
 tuner = kt.Hyperband(
